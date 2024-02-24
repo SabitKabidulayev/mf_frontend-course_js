f1d71318ed87acc1f5a8d5af465a0205010fc6b2
SabitKabidulayev <kabidulayevs@gmail.com>
2024-02-24 20:29:22 +0600

    js-hw-13

--- js-hw-13-async-code/task-4/script.js
+++ js-hw-13-async-code/task-4/script.js
@@ -12,9 +12,5 @@ xhr.onload = function() {
     const response = xhr.response
     data = JSON.parse(response).slice(0, 20)
 
-    const header = document.createElement('h1')
-    
-    header.innerText = data.title
-
-    taskHeaderDiv.appendChild(header)
+   
 }
