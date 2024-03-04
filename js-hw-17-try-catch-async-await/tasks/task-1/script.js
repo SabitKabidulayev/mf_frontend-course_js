c4eb20852fe396f1d05bc2ebc652f15d6b82c250
SabitKabidulayev <kabidulayevs@gmail.com>
2024-03-04 18:51:49 +0500

    js-hw-17-task-1

--- js-hw-17-try-catch-async-await/tasks/task-1/script.js
+++ js-hw-17-try-catch-async-await/tasks/task-1/script.js
@@ -6,10 +6,16 @@ async function getData(url) {
 
 const urlUsers = 'https://jsonplaceholder.typicode.com/users'
 
+const usersList = document.querySelector('#users-list')
+
 async function getUsers() {
     try {
         const data = await getData(urlUsers)
-        console.log(data)
+        for (user of data) {
+          const userElement = document.createElement('li')
+          userElement.innerText = user.name
+          usersList.appendChild(userElement)
+        }
     } catch (error) {
         console.log(error.message)
     }
