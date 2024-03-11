7044eaeecaecfb124017d3ab75f6ea51fa985e31
SabitKabidulayev <kabidulayevs@gmail.com>
2024-03-11 11:30:19 +0500

    js-hw-17-task-2

--- js-hw-17-try-catch-async-await/tasks/task-2/script.js
+++ js-hw-17-try-catch-async-await/tasks/task-2/script.js
@@ -10,13 +10,16 @@ const searchButton = document.querySelector('#search-button')
 
 searchButton.addEventListener('click', () => {
     searchVideo()
-} )
+})
 
 
 async function searchVideo() {
     try {
         const searchAPI = `https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCat_2WJTJcsdgIkjvuNZ8KuaVtqOyNsuk&q=${searchInput.value}&type=video`
-        
+        const data = await getData(searchAPI)
+        console.log(data.items[0].id.videoId)
+
+
     } catch (error) {
         console.log(error.message)
     }
